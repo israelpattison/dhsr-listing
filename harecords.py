1f92470eec3fea98509b9e1b48956de37349ad0d
Israel J Pattison <pattison@usa.com>
2017-01-05 00:06:10 -0500

    lots of changes, down to just one class for dhsr facility listing

--- harecords.py
+++ harecords.py
@@ -1,57 +1,81 @@
-import requests
-import cStringIO
-import re
+from requests import get
+from csv import DictReader
+import re, cStringIO
 
-class HaData:
+# The base URI of the NC DHSR website
+DHSR_SITE = 'https://www2.ncdhhs.gov/dhsr'
 
-    def __init__(self, url):
-        self.setUrl(url)
-        self.retrieveData()
+# The URI for the facility listing header file on the DHSR website
+DHSR_HAHEADER_FILE_URL = DHSR_SITE + '/data/ha-header.txt'
 
-    def setUrl(self, url):
-        self.url = url
+# The URI for the facility listing records on the DHSR website
+DHSR_DATA_FILE_URL = DHSR_SITE + '/data/ha.txt'
 
-    def getUrl(self):
-        return self.url
+class DhsrFacilityListing:
 
-    def retrieveData(self):
+    def __init__(self):
+
+        # Retrieve and store the facility listings from the DHSR
+        # website when we create a new listing object
+        self.retrieveRecords()
+
+    # A method to retrieve and return contents from the web for both
+    # the header file and the facility listing
+    def retrieve(self, url):
+
+        # Use HTTP GET to retrieve the text contents of the file based
+        # on the URI
+        file = get(url).text
+
+        # Create a new StringIO object so we can stream the file
+        # contents into a variable
+        # Necessary to remove Unicode encoding per Python 2.7
+        # shortcomings
         output = cStringIO.StringIO()
-        dataFile = requests.get(self.url).text
-        output.write(dataFile)
-        contents = output.getvalue()
-        output.close()
-        self.records = contents.split('\r\n')
+
+        # Stream the contents of the file into the StringIO object
+        output.write(file)
+
+        # Return the value of the StringIO object and save it in the
+        # content variable
+        content = output.getvalue()
+
+        # Close the StringIO object
+        output.close
+
+        # Return the processed content from the file retrieved from
+        # the web
+        return content
+
+    # A method to retrieve the header file from the DHSR website
+    # and return a the field names as a list
+    def retrieveFieldNames(self, url):
+
+        # Retrieve and store the contents of the facilities listing
+        # header file from the DHSR website
+        content = self.retrieve(url)
+
+        # Process the file to find the line containing the field names
+        lines = content.split('\r\n')
+        pattern = re.compile("^(\w+,)+\w+$")
+        for line in lines:
+            if pattern.match(line):
+                fieldNames = line.split(',')
+
+        # Return a list of field names to the caller
+        return fieldNames
+
+    def retrieveRecords(self):
+
+        # Retrieve a list of field names from the DHSR website
+        self.fieldNames = self.retrieveFieldNames(DHSR_HAHEADER_FILE_URL)
+
+        # Retrieve the facility records from the DHSR website
+        self.facilityRecords = self.retrieve(DHSR_DATA_FILE_URL)
+
+        # Save a list of dictionaries containing the facility records
+        #TODO: figure out how to return a dictionary instead of a DictReader object
+        self.records = DictReader(self.facilityRecords, fieldnames=self.fieldNames)
 
     def getRecords(self):
         return self.records
-
-class HaHeaders:
-
-    def __init__(self, url):
-        self.setUrl(url)
-        self.retrieveHeaders()
-
-    def setUrl(self, url):
-        self.url = url
-
-    def getUrl(self):
-        return self.url
-
-    def retrieveHeaders(self):
-        output = cStringIO.StringIO()
-        headerFile = requests.get(self.url).text
-        output.write(headerFile)
-        contents = output.getvalue()
-        output.close()
-        lines = contents.split('\r\n')
-        pattern = re.compile("^(\w+,)+\w+$")
-
-        for line in lines:
-            if pattern.match(line):
-                self.fields = line.split(',')
-
-    def getFieldNames(self):
-        return self.fields
-
-    def printFieldNames(self):
-        print self.fields
--- playground.py
+++ playground.py
@@ -1,45 +1,7 @@
 from harecords import *
 import csv
 
-DHSR_HAHEADER_FILE_URL = 'https://www2.ncdhhs.gov/dhsr/data/ha-header.txt'
-DHSR_DATA_FILE_URL = 'https://www2.ncdhhs.gov/dhsr/data/ha.txt'
+listing = DhsrFacilityListing()
 
-dhsrHaheaderFile = HaHeaders(DHSR_HAHEADER_FILE_URL)
-fields = dhsrHaheaderFile.getFieldNames()
+print listing
 
-dhsrDataFile = HaData(DHSR_DATA_FILE_URL)
-records = dhsrDataFile.getRecords()
-
-reader = csv.DictReader(records, fieldnames=fields)
-
-print(reader[76])
-
-# import requests
-# import StringIO
-# import csv
-#
-# headerRequest = requests.get('https://www2.ncdhhs.gov/dhsr/data/ha-header.txt')
-# dataRequest = requests.get('https://www2.ncdhhs.gov/dhsr/data/ha.txt')
-#
-# headersString = headerRequest.text.split('\r\n')[6]
-# dataString = dataRequest.text
-#
-# fieldNames = headersString.split(',')
-#
-#
-# ---
-# from requests import get
-# import cStringIO
-# import re
-#
-# file = get('https://www2.ncdhhs.gov/dhsr/data/ha-header.txt')
-# output = cStringIO.StringIO()
-# output.write(file.text)
-# contents = output.getvalue()
-# output.close()
-# lines = contents.split('\r\n')
-# pattern = re.compile("^(\w+,)+\w+$")
-# for line in lines:
-#     if pattern.match(line):
-#         fields = line.split(,)
-#
